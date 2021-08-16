dd3e4ee87a58fcf29d7a3ef0485f46cef161c2ca
ManukhovStepan <63719382+ManukhovStepan@users.noreply.github.com>
2021-08-17 00:08:21 +0300

    Update CutterFiveFou.C
    Mistakes

--- Spectre/CutterFiveFou.C
+++ Spectre/CutterFiveFou.C
@@ -12,9 +12,6 @@
 #include "TH1F.h"
 #include "TH1D.h"
 #include "TH2F.h"
-#include "TGraph.h"
-#include "TGraphErrors.h"
-#include "TMultiGraph.h"
 #include "TCanvas.h"
 #include "TVector3.h"
 #include "TMath.h"
@@ -27,7 +24,6 @@ const Char_t *defaultOutFile="Cutting.root";
 
 using namespace std;
 
- // Bose-Einstein fitting function
 int CutterFiveFou(const Char_t *inFile = "star/data56/reco/production_14p5GeV_2019/ReversedFullField/P19ic/2019/118/20118006/st_physics*.picoDst.root",   const Char_t *outFileName=defaultOutFile)
 {
     // Инициализирую вспомогательные переменные, открываю файл
@@ -45,7 +41,7 @@ int CutterFiveFou(const Char_t *inFile = "star/data56/reco/production_14p5GeV_20
     Double_t Vx;
     Double_t Vy;
 
-    Double_t nFitPOver;
+    Double_t nFitPOver; //!!!
     Double_t Yields;
 
     TH1D *hVertexZ;
@@ -58,9 +54,9 @@ int CutterFiveFou(const Char_t *inFile = "star/data56/reco/production_14p5GeV_20
     TH1D *hDCAmagn;
     TH2D *hDCAxy;
     TH2D *hGPtOverPPt;
-    TH2D *hNFitPVsEta2D;
-    TH2D *hSpectraVsEta2D;
-    TH2D *hSpectraVsNFitP2D;
+    TH2D *hNFitPVsEta2D; ///!!
+ //   TH2D *hSpectraVsEta2D; //!!
+  //  TH2D *hSpectraVsNFitP2D; //!!
 
     hVertexZ 		= new TH1D("Vertex Z","Vertex Z destribution",20, -50., 50.);
     hVertexXY 		= new TH2D("Vertex XY","Vertex XY destribution",40, -0.5, 0.5, 40, -0.5, 0.5);
@@ -72,7 +68,7 @@ int CutterFiveFou(const Char_t *inFile = "star/data56/reco/production_14p5GeV_20
     hDCAxy		= new TH2D("hDCAxy", "DCA XY destribution", 50, -5, 5, 50, -5, 5);
     hDCAmagn		= new TH1D("hDCAmagn", "DCA magnitude destribution", 50, 0.,2);
     hGPtOverPPt		= new TH2D("hGPtOverPPt", "Global track vs. primary track P_{T} destribution", 50, 0.01, 4, 50, 0.01, 4);
-    hNFitPVsEta2D		= new TH2D("hNFitPVsEta2D", "Number of fit points vs. pseudo rapidity", 50, -1.5, 1.5, 50, 0., 50.);
+    hNFitPVsEta2D		= new TH2D("hNFitPVsEta2D", "Number of fit points vs. pseudo rapidity", 50, -1.5, 1.5, 50, 0., 50.); //!!
 //    hSpectraVsEta2D	= new TH2D("hSpectraVsEta2D", "d^{2}N/2piP_{T}dP_{T}dEta distribution vs. pseudo rapidity", 20, -1.5, 1.5, 30, 0.2, 1.9);
 //    hSpectraVsNFitP2D	= new TH2D("hSpectraVsNFitP2D", "d^{2}N/2piP_{T}dP_{T}dEta distribution vs. number of fit points", 50, 0., 50., 30, 0.2, 1.9);
 
@@ -99,7 +95,7 @@ int CutterFiveFou(const Char_t *inFile = "star/data56/reco/production_14p5GeV_20
 
     Double_t posYields=0;
     Double_t negYields=0;
-    Double_t events2average = 0.;
+    Double_t events2average = 0.; //!!
     Int_t NumBins = 180;
     TString hPos_names[9];
     TH1D *hPosSpectra[9];
@@ -118,7 +114,7 @@ int CutterFiveFou(const Char_t *inFile = "star/data56/reco/production_14p5GeV_20
       };
 
  // Начинаю считывание по событиям
-  picoReader->SetStatus("Event*", 1);
+  picoReader->SetStatus("Event*", 1); //!!
   Long64_t events2read = picoReader->chain()->GetEntries();
   //if (events2read > 5000) events2read = 5000;
    for(Long64_t iEvent=0; iEvent<events2read; iEvent++) {
@@ -140,13 +136,14 @@ int CutterFiveFou(const Char_t *inFile = "star/data56/reco/production_14p5GeV_20
      refmultCorrUtil-> init(event->runId());
      refmultCorrUtil-> initEvent(RefMult, event->primaryVertex().Z(), event->ZDCx());
      cent9 = refmultCorrUtil->getCentralityBin9();
-     Bool_t isBadRun = refmultCorrUtil->isBadRun(runid);
-     
+    
+     Bool_t isBadRun = refmultCorrUtil->isBadRun(runid);    
      if(isBadRun)
      continue;
      
-if (cent9 > -1)
-{
+if (cent9 < 0) 
+ continue;
+
 
  // Счтиываю каждый трэк
     for(Int_t iTrk=0; iTrk<dst->numberOfTracks(); iTrk++) {
@@ -160,18 +157,18 @@ if (cent9 > -1)
       (abs(picoTrack->gDCA(event->primaryVertex()).Y()) < 5) &&
       (picoTrack->gDCA(event->primaryVertex()).Mag() < 2) &&
       (picoTrack->gMom().Pt() < 4) &&
-      (picoTrack->pMom().Pt() < 4))
+      (picoTrack->pMom().Pt() < 4)) //!!!
       {
-    nFitPOver = ((picoTrack->nHitsFit()) / (picoTrack->nHitsMax()));
+    nFitPOver = ((Double_t)(picoTrack->nHitsFit()) / (Double_t)(picoTrack->nHitsMax())); //!!
     
     hNFitPoints->Fill(picoTrack->nHitsFit());
-    hNFitPtOverPossPt->Fill(nFitPOver);
+    hNFitPtOverPossPt->Fill((Double_t)nFitPOver);
     hNFitdEdx->Fill(picoTrack->nHitsDedx());
     hDCAz->Fill(picoTrack->gDCA(event->primaryVertex()).Z());
     hDCAxy->Fill(picoTrack->gDCA(event->primaryVertex()).X(), picoTrack->gDCA(event->primaryVertex()).Y());
     hDCAmagn->Fill(picoTrack->gDCA(event->primaryVertex()).Mag());
     hGPtOverPPt->Fill(picoTrack->gMom().Pt(), picoTrack->pMom().Pt());
-    if( abs(picoTrack->pMom().Eta())> 0.005)
+    if( abs(picoTrack->pMom().Eta())> 0.005) //!!!
     hNFitPVsEta2D->Fill(picoTrack->pMom().Eta(),picoTrack->nHitsFit());
     }
 
@@ -180,12 +177,12 @@ if (cent9 > -1)
 
     if(   	 (picoTrack->isPrimary())    &&
             (abs(event->primaryVertex().Z())<30.) 	&&
-            (rV<0.25)                      &&
+            (rV<0.25)                      &&  //!!
             (abs(picoTrack->pMom().Eta())<0.5) &&
             (picoTrack->gDCA(event->primaryVertex()).Mag()<1.) &&
             (picoTrack->nHitsFit()>15.) &&
-            (((picoTrack->nHitsFit())/(picoTrack->nHitsMax()))>0.24) &&
-            (picoTrack->nHitsDedx()>13.) &&
+           // (((picoTrack->nHitsFit())/(picoTrack->nHitsMax()))>0.24) && //!!
+            (picoTrack->nHitsDedx()>13.) && //!!
             (picoTrack->pMom().Pt()>0.2) &&
             ((picoTrack->gMom().Pt() / picoTrack->pMom().Pt())>0.7) &&
             ((picoTrack->gMom().Pt() / picoTrack->pMom().Pt())<1.4285) )  {
@@ -196,21 +193,18 @@ if (cent9 > -1)
              if(picoTrack->charge()>0)
              {
              posYields = 1./((2.*3.14159) *(picoTrack ->pMom().Pt()));
-             //PosMomentum[cent9] = picoTrack ->pMom().Pt();
              hPosSpectra[cent9]-> Fill(picoTrack ->pMom().Pt(), posYields);
 
              }
              if(picoTrack->charge()<0)
              {
              negYields = 1./((2.*3.14159) *(picoTrack ->pMom().Pt()));
-             //NegMomentum[cent9] = picoTrack ->pMom().Pt();
              hNegSpectra[cent9]-> Fill(picoTrack ->pMom().Pt(), negYields);
              }
 }
 }
  /* end for(Int_t iTrk=0; iTrk<dst->numberOfTracks(); iTrk++)
  */
-}
 hRefMult ->Fill(RefMult);
 hVertexZ ->Fill(event->primaryVertex().Z());
 hVertexXY->Fill(event->primaryVertex().X(), event->primaryVertex().Y());
@@ -235,8 +229,7 @@ hVertexXY->Fill(event->primaryVertex().X(), event->primaryVertex().Y());
       hDCAxy->Write();
       hDCAmagn->Write();
       hGPtOverPPt->Write();
-      hNFitPVsEta2D->Write();
-     // ofile.Write();
+      hNFitPVsEta2D->Write(); //!!
      ofile.Close();
      l.Delete();
      delete hRefMult;
